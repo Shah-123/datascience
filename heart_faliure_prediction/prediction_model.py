93da40739b547b35e4deaed2d04de80c5aafdbb9
Shahkar Ahmad <123003961+Shah-123@users.noreply.github.com>
2024-05-23 20:00:59 +0500

    Update prediction_model.py

--- heart_faliure_prediction/prediction_model.py
+++ heart_faliure_prediction/prediction_model.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, classification_report
 import seaborn as sns
 import matplotlib.pyplot as plt
 
